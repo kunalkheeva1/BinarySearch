594f332b3e44dad33e7851217c0269cffa8c4854
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-07 21:13:19 -0700

    created a method to perform a binary search, it can only happen when the array is sorted.

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,19 @@
 public class BinarySearch {
-    
+    public static int binarySearch(int arr[], int n, int k){
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (high + low)/2;
+            if(arr[mid]== k){
+                return mid;
+            }
+            else if(arr[mid]< k){
+                low = mid +1;
+            }else{
+                high = mid-1;
+            }
+        }return -1;
+    }
 
     public static void main(String[] args) {
 
